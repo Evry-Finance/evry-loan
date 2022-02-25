0d3ac8afc99514b7ac488097f867ea79cf50a4de
evry-dev <89249087+evry-dev@users.noreply.github.com>
2022-02-25 16:30:29 +0700

    remove some no use code

--- contracts/interfaces/IStakingPools.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.9;
-
-interface IStakingPools  {
-  function deposit(uint256 _poolId, uint256 _depositAmount) external;
-  function withdraw(uint256 _poolId, uint256 _withdrawAmount) external;
-  function claim(uint256 _poolId) external;
-}
-
